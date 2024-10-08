d0c57dadeccf4df3c04a026eb8022dc105022ff8
Shunsuke Ito <t22j209ai@ous.jp>
2024-10-09 08:32:46 +0900

    Natas33

---
+++ Natas/Natas33/Natas33.php
@@ -0,0 +1,23 @@
+<?php
+    // graz XeR, the first to solve it! thanks for the feedback!
+    // ~morla
+    class Executor{
+        private $filename="test.php"; 
+        private $signature='adeafbadbabec0dedabada55ba55d00d';
+        private $init=False;
+
+        // create new Phar
+        $phar = new Phar('natas.phar');
+        $phar->startBuffering();
+        $phar->addFromString('natas.txt', 'text');
+        $phar->setStub('<?php __HALT_COMPILER(); ? >');
+        // add object of any class as meta data
+        class AnyClass {}
+        $object = new AnyClass;
+        $object->data = 'rips';
+        $phar->setMetadata($object);
+        $phar->stopBuffering();
+            }
+?>
+
+シグネチャ・チェックはデストラクト処理を実行するのでは？そうすると、pharファイルのデシリアライズ関数が呼び出されます。
